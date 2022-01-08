0017ec0b740970aa68e83c5a96b00a18a022d55d
Alexandre Pinon <alexandre.pinon@epitech.eu>
2022-01-08 21:00:55 +0100

    feat: added user model

---
+++ src/domain/entities/User.ts
@@ -0,0 +1,13 @@
+import { Role } from '@/custom-types'
+
+export type User = {
+  id: string | number
+  firstName: string
+  lastName: string
+  username: string
+  email: string
+  password: string
+  role: Role
+  description: string
+  socialLinks: string[]
+}
---
+++ src/types/Role.ts
@@ -0,0 +1,5 @@
+export enum Role {
+  'SUPERADMIN' = 0,
+  'ADMIN' = 1,
+  'USER' = 2,
+}
